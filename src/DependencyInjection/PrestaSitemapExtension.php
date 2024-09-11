6570e6d85c1f092ee7a746b1cd04249c2bfa9df1
daFish81 <ms@pond-solutions.de>
2024-09-11 17:11:17 +0200

    fix: move parameter setting above service loading

--- src/DependencyInjection/PrestaSitemapExtension.php
+++ src/DependencyInjection/PrestaSitemapExtension.php
@@ -30,15 +30,15 @@ class PrestaSitemapExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
-        $loader->load('services.xml');
-
         $container->setParameter($this->getAlias() . '.dump_directory', (string)$config['dump_directory']);
         $container->setParameter($this->getAlias() . '.timetolive', (int)$config['timetolive']);
         $container->setParameter($this->getAlias() . '.sitemap_file_prefix', (string)$config['sitemap_file_prefix']);
         $container->setParameter($this->getAlias() . '.items_by_set', (int)$config['items_by_set']);
         $container->setParameter($this->getAlias() . '.defaults', $config['defaults']);
         $container->setParameter($this->getAlias() . '.default_section', (string)$config['default_section']);
+
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../../config'));
+        $loader->load('services.xml');
 
         if (true === $config['route_annotation_listener']) {
             $loader->load('route_annotation_listener.xml');
